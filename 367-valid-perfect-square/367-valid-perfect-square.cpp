2b823c493e56a0e769db24ad8144a821f3169416
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-07-10 19:33:11 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (86.18%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    bool isPerfectSquare(int x) {
-         long r = x;
-    while (r*r > x)
-        r = (r + x/r) / 2;
-    return r*r == x;
+    bool isPerfectSquare(int n) {
+        int p=sqrt(n);
+        return p*p==n;
     }
 };
